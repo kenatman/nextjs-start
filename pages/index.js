d8c121b01406845a625a567c2e30826c11a9b802
Yunsu <kenatman0723@gmail.com>
2022-06-01 20:43:59 +0900

    next.config.js : redirects & rewrites : rewrites를 이용한 API 감추기

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "554b6cc28b5c520e4001c4f32336fd25";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
       const { results } = await (
         await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+          '/api/movies'
         )
       ).json();
       setMovies(results);
