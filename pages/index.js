679612d5f6f42a6b3c3c3f5786c66fc5701348d2
Yunsu <kenatman0723@gmail.com>
2022-06-01 11:00:01 +0900

    홈화면 영화정보 세팅

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "554b6cc28b5c520e4001c4f32336fd25";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="HOME" />
-      <h1>HOME</h1>
+      <div>
+        {movies?.map((movie) => (
+          <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
